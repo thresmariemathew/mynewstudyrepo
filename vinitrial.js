739f39b8b7e307b81c2fe10e310dd758c856b24b
thresmariemathew <thresmariemathew@gmail.com>
2025-03-09 23:20:38 +0530

    this is vini method trial 1

---
+++ vinitrial.js
@@ -0,0 +1,11 @@
+// this is my trial page and  
+// this is  to study git 
+
+
+//push .
+describe("new",function(){
+    it('hello',async function () {
+        expect(console.log("hello"));
+    })
+    
+    });
